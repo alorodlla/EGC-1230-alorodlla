96e5528e2f7ffe42efad38a42a3ba5696ae186c9
alorodlla <alorodlla@alum.us.es>
2021-02-12 11:41:23 +0100

    ADD Añadido nuevo travis para correr satisfactoriamente

--- decide/local_settings.py
+++ decide/local_settings.py
@@ -30,9 +30,10 @@ BASEURL = 'http://10.5.0.1:8000'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
+        'NAME': 'usuariodb',
+        'USER': 'usuario',
+        'PASSWORD': 'contraseña'
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
